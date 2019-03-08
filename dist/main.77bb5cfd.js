208c248d970f6782454ad5bf8243a1d8489b1a2f
Oscar Rodriguez <oscarrobertrodriguez@gmail.com>
2019-03-07 22:24:40 -0600

    add styles for carousel section

--- dist/main.77bb5cfd.js
+++ dist/main.77bb5cfd.js
@@ -176,7 +176,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"/Users/oscar/Desktop/css grid practice/skateboard-site/assets/urban-skateboard-svgrepo-com.svg":[["urban-skateboard-svgrepo-com.009cf388.svg","assets/urban-skateboard-svgrepo-com.svg"],"assets/urban-skateboard-svgrepo-com.svg"],"_css_loader":"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
